ba62d3a2ba3bc14375bb81b2dbac97de5a658219
Pranshu Verma <105149813+Pransh013@users.noreply.github.com>
2023-08-14 12:54:15 +0530

    Time: 3 ms (53.68%), Space: 48.7 MB (30.22%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,21 +1,8 @@
 class Solution {
     public int majorityElement(int[] arr) {
         int n = arr.length;
-        int max = Integer.MIN_VALUE;
-        int ans = Integer.MIN_VALUE;
-        for(int i = 0;i<n;i++){
-            int count = 1;
-            for(int j = 1;j<n;j++){
-                if(arr[i]==arr[j])
-                    count++;
-            }
-             
-            if(count>=(n+1)/2){
-                max = count;
-                ans = arr[i];
-                break;
-            }
-        }
+        Arrays.sort(arr);
+        int ans = arr[n/2];
         return ans;
     }
 }
