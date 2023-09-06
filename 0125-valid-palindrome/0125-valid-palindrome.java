7cce24f709d12aaa59739d5801261a2781af8119
Pranshu Verma <105149813+Pransh013@users.noreply.github.com>
2023-09-06 20:26:22 +0530

    Time: 3 ms (82.11%), Space: 44 MB (34.61%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -5,26 +5,32 @@ class Solution {
             return true;
         }
 
+        str = str.toLowerCase();
+        StringBuilder sb = new StringBuilder();
+        for(int i=0; i<str.length(); i++) {
+            char ch = str.charAt(i);
+            if(isAlphaNumeric(ch)) sb.append(ch);
+        }
+        
+        str = sb.toString();
+        
         int i = 0;
         int j = str.length() - 1;
-
         while (i < j) {
             char chL = str.charAt(i);
             char chR = str.charAt(j);
-    
-            if (!Character.isLetterOrDigit(chL)) {
-                i++;
-            } else if (!Character.isLetterOrDigit(chR)) {
-                j--;
-            } else {
-                if (Character.toLowerCase(chL) != Character.toLowerCase(chR)) {
-                    return false;
-                }
-                i++;
-                j--;
+            if (chL != chR) {
+                return false;
             }
+            i++;
+            j--;
         }
 
         return true;
+    }
+    public boolean isAlphaNumeric(char ch) {
+        if(ch >= 'a' && ch <= 'z') return true;
+        if(ch >= '0' && ch <= '9') return true;
+        return false;
     }
 }
