9dc9b7818400781ec5cff45f9f56af6db8b9d346
Pranshu Verma <105149813+Pransh013@users.noreply.github.com>
2023-09-06 22:48:03 +0530

    Time: 3 ms (96.19%), Space: 43.9 MB (36.66%) - LeetHub

--- 1769-minimum-number-of-operations-to-move-all-balls-to-each-box/1769-minimum-number-of-operations-to-move-all-balls-to-each-box.java
+++ 1769-minimum-number-of-operations-to-move-all-balls-to-each-box/1769-minimum-number-of-operations-to-move-all-balls-to-each-box.java
@@ -1,16 +1,25 @@
 class Solution {
-    public int[] minOperations(String str) {
-        int n = str.length();
+    public int[] minOperations(String boxes) {
+        int n = boxes.length();
         int[] ans = new int[n];
         
+        int[] pre = new int[n];
+        int[] suff = new int[n];
+        
+        int countOfBalls = boxes.charAt(0) - '0';
+        for(int i=1; i<n; i++) {
+            pre[i] = pre[i-1] + countOfBalls;
+            if(boxes.charAt(i) == '1') countOfBalls++;
+        }
+        
+        countOfBalls = boxes.charAt(n-1) - '0';
+        for(int i=n-2; i>=0; i--) {
+            suff[i] = suff[i+1] + countOfBalls;
+            if(boxes.charAt(i) == '1') countOfBalls++;
+        }
+        
         for(int i=0; i<n; i++) {
-            int steps = 0;
-            
-            for(int j=0; j<n; j++) {
-                if(j != i && str.charAt(j) == '1')
-                    steps += Math.abs(j - i);
-            }
-            ans[i] = steps;
+            ans[i] = pre[i] + suff[i];
         }
         return ans;
     }
