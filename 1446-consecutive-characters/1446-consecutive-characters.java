875f812973391db681351cbf095cfff794f8f023
Pranshu Verma <105149813+Pransh013@users.noreply.github.com>
2023-09-11 21:17:44 +0530

    Time: 3 ms (8.35%), Space: 41 MB (52.14%) - LeetHub

--- 1446-consecutive-characters/1446-consecutive-characters.java
+++ 1446-consecutive-characters/1446-consecutive-characters.java
@@ -1,10 +1,8 @@
 class Solution {
     public int maxPower(String str) {
-        if(str.length() == 1) {
-            return 1;
-        }
+        
         int i=0, j=i+1;
-        int maxLen = -1;
+        int maxLen = 1;
         while(j < str.length()) {
             char ch = str.charAt(i);
             if(str.charAt(j) != ch)
