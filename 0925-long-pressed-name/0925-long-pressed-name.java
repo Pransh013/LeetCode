f510d1c369bab68e66e836911f131a726afa463b
Pranshu Verma <105149813+Pransh013@users.noreply.github.com>
2023-09-12 23:50:37 +0530

    Time: 0 ms (100.00%), Space: 40.4 MB (61.34%) - LeetHub

---
+++ 0925-long-pressed-name/0925-long-pressed-name.java
@@ -0,0 +1,31 @@
+class Solution {
+    public boolean isLongPressedName(String s1, String s2) {
+        int l1 = s1.length(), l2 = s2.length();
+        if((l2 < l1) || (s1.charAt(l1 - 1) != s2.charAt(l2 - 1)) || (s1.charAt(0) != s2.charAt(0)))
+            return false;
+        
+        int i=0, j=0;
+        
+        while(i < l1) {
+            if(j == l2)
+                return false;
+            char curr = s1.charAt(i);
+            
+            if(s2.charAt(j) == curr) {
+                i++;
+                j++;
+            } else {
+                if(j == 0 || s2.charAt(j) == s2.charAt(j-1))
+                    j++;
+                else 
+                    return false;
+            }
+        }
+        while(j != l2) {
+            if(s2.charAt(j) != s2.charAt(j-1))
+                return false;
+            j++;
+        }
+        return true;
+    }
+}
