53dcadfe4c5a9823f5b25e7a1604d0706274a5b0
Pranshu Verma <105149813+Pransh013@users.noreply.github.com>
2023-09-14 01:53:52 +0530

    Time: 0 ms (100.00%), Space: 43 MB (38.57%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.java
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.java
@@ -2,9 +2,37 @@ class Solution {
     public boolean search(int[] nums, int k) {
         int n = nums.length;
     
-        for(int i=0; i<n; i++) {
-            if(nums[i] == k)
+        int l=0, h=n-1;
+        
+        while(l <= h) {
+            int m = (l + h) / 2;
+            
+            if(nums[m] == k)
                 return true;
+            
+//             the only case which is stopping us from implementing search in RSA-I solution here 
+            if(nums[m] == nums[l] && nums[m] == nums[h]) {
+                l++;
+                h--;
+                continue;
+            }
+            
+//             check if right half is sorted
+            else if(nums[h] >= nums[m]) {
+//                 check if target lies in right sorted half
+                if(k >= nums[m] && k <= nums[h])
+                    l = m + 1;
+                else
+                    h = m - 1;
+                
+//             check if left half is sorted
+            } else {
+//                 check if target lies in left sorted half
+                if(k >= nums[l] && k <= nums[m])
+                    h = m - 1;
+                else
+                    l = m + 1;
+            }
         }
         return false;
     }
