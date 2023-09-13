c5b8e552493a68f44e24a73f213f8498448996e8
Pranshu Verma <105149813+Pransh013@users.noreply.github.com>
2023-09-13 16:15:15 +0530

    Time: 0 ms (100.00%), Space: 44.2 MB (89.08%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,41 +1,39 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        int[] ans = new int[2];
-        
         int n = nums.length;
-        int start = -1, end = -1;
-        int l = 0, h = n-1;
         
-        while(l <= h) {
-            int mid = (l + h) / 2;
-            if(nums[mid] == target) {
-                start = mid;
-                h = mid - 1;
-            } else if(nums[mid] < target) {
-                l = mid + 1;
-            } else {
-                h = mid - 1;
-            }
+        int lb = lowerBound(nums, n, target);
+        if(lb == n || nums[lb] != target)
+            return new int[]{-1, -1};
+        int ub = upperBound(nums, n, target);
+        
+        return new int[]{lb, ub - 1};
+    }
+    public int lowerBound(int[] arr, int n, int k) {
+        int l=0, h=n;
+        
+        while(l < h) {
+            int m = (l + h) / 2;
+            
+            if(arr[m] < k)
+                l = m + 1;
+            else
+                h = m;
         }
+        return h;
+    }
+    
+    public int upperBound(int[] arr, int n, int k) {
+        int l=0, h=n;
         
-        l = 0;
-        h = n-1;
-        
-        while(l <= h) {
-            int mid = (l + h) / 2;
-            if(nums[mid] == target) {
-                end = mid;
-                l = mid + 1;
-            } else if(nums[mid] < target) {
-                l = mid + 1;
-            } else {
-                h = mid - 1;
-            }
+        while(l < h) {
+            int m = (l + h) / 2;
+            
+            if(arr[m] <= k)
+                l = m + 1;
+            else
+                h = m;
         }
-        
-        ans[0] = start;
-        ans[1] = end;
-        
-        return ans;
+        return h;
     }
 }
