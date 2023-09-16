8f71eabcbdc66475cc60713dcbd73fb7087449d9
Pranshu Verma <105149813+Pransh013@users.noreply.github.com>
2023-09-17 01:37:15 +0530

    Time: 1 ms (99.32%), Space: 44.7 MB (28.84%) - LeetHub

--- 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.java
+++ 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.java
@@ -1,14 +1,11 @@
 class Solution {
     public int[] kWeakestRows(int[][] mat, int k) {
-        
         int[] arr = new int[mat.length];
         int idx = 0;
         for(int[] rows : mat) {
             int low = 0, high = rows.length - 1, ans = -1;
-            
             while(low <= high) {
                 int mid = low + ((high - low) / 2);
-                
                 if(rows[mid] == 1) {
                     ans = mid;
                     low = mid + 1;
@@ -16,11 +13,8 @@ class Solution {
                     high = mid - 1;
                 }
             }
-            arr[idx] = ans + 1;
-            System.out.print(arr[idx] + " ");
-            idx++;
+            arr[idx++] = ans + 1;
         }
-        
         int ans[] = new int[k];
         for(int i = 0; i < k; i++){
             int min = Integer.MAX_VALUE;
