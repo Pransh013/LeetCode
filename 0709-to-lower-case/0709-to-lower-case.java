1c03787ba90e1d09f0a78be3f1b8924f1e3ca9ed
Pranshu Verma <105149813+Pransh013@users.noreply.github.com>
2023-09-18 19:04:28 +0530

    Time: 0 ms (100.00%), Space: 40.6 MB (56.18%) - LeetHub

--- 0709-to-lower-case/0709-to-lower-case.java
+++ 0709-to-lower-case/0709-to-lower-case.java
@@ -1,8 +1,7 @@
 class Solution {
     public String toLowerCase(String s) {
         StringBuilder sb = new StringBuilder();
-        for(int i=0; i<s.length(); i++) {
-            char ch = s.charAt(i);
+        for(char ch : s.toCharArray()) {
             if(ch >= 65 && ch <= 90)
                 ch += 32;
             sb.append(ch);
