0a6dfb7d869eb526e83baaf47e87c804ca372cb2
Pranshu Verma <105149813+Pransh013@users.noreply.github.com>
2023-09-19 15:55:54 +0530

    Time: 0 ms (100.00%), Space: 40.3 MB (86.72%) - LeetHub

--- 0844-backspace-string-compare/0844-backspace-string-compare.java
+++ 0844-backspace-string-compare/0844-backspace-string-compare.java
@@ -1,16 +1,45 @@
 class Solution {
-    public boolean backspaceCompare(String s, String t) {
+    public boolean backspaceCompare(String S, String T) {
+        int S_pointer = S.length() - 1;
+        int T_pointer = T.length() - 1;
         
-        return reformed(s).equals(reformed(t));
-    }
-    private String reformed(String str) {
-        Stack<Character> st = new Stack();
-        for(char ch : str.toCharArray()) {
-            if(ch != '#')
-                st.push(ch);
-            else if(!st.empty())
-                st.pop();
-        }
-        return String.valueOf(st);
+        int S_skips = 0, T_skips = 0;
+        
+        while(S_pointer >= 0 || T_pointer >= 0) {
+            
+            while(S_pointer >= 0) {
+                if(S.charAt(S_pointer) == '#') {
+                    S_skips++;
+                    S_pointer--;
+                } else if(S_skips > 0) {
+                    S_pointer--;
+                    S_skips--;
+                } else {
+                    break;
+                }
+            }
+            
+            while(T_pointer >= 0) {
+                if(T.charAt(T_pointer) == '#') {
+                    T_skips++;
+                    T_pointer--;
+                } else if(T_skips > 0) {
+                    T_pointer--;
+                    T_skips--;
+                } else {
+                    break;
+                }
+            }
+            
+            if(S_pointer >= 0 && T_pointer >= 0 && S.charAt(S_pointer) != T.charAt(T_pointer))
+                return false;
+            
+            if((S_pointer >= 0) != (T_pointer >= 0))
+                return false;
+            
+            S_pointer--;
+            T_pointer--;
+        } 
+        return true;
     }
 }
