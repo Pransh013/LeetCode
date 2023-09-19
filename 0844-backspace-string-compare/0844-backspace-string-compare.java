b89d9514f0603239df1ce85ad17b5a6eb30f2125
Pranshu Verma <105149813+Pransh013@users.noreply.github.com>
2023-09-19 15:56:50 +0530

    Time: 0 ms (100.00%), Space: 40.6 MB (50.49%) - LeetHub

--- 0844-backspace-string-compare/0844-backspace-string-compare.java
+++ 0844-backspace-string-compare/0844-backspace-string-compare.java
@@ -7,29 +7,8 @@ class Solution {
         
         while(S_pointer >= 0 || T_pointer >= 0) {
             
-            while(S_pointer >= 0) {
-                if(S.charAt(S_pointer) == '#') {
-                    S_skips++;
-                    S_pointer--;
-                } else if(S_skips > 0) {
-                    S_pointer--;
-                    S_skips--;
-                } else {
-                    break;
-                }
-            }
-            
-            while(T_pointer >= 0) {
-                if(T.charAt(T_pointer) == '#') {
-                    T_skips++;
-                    T_pointer--;
-                } else if(T_skips > 0) {
-                    T_pointer--;
-                    T_skips--;
-                } else {
-                    break;
-                }
-            }
+            S_pointer = reformed(S, S_skips, S_pointer);
+            T_pointer = reformed(T, T_skips, T_pointer);  
             
             if(S_pointer >= 0 && T_pointer >= 0 && S.charAt(S_pointer) != T.charAt(T_pointer))
                 return false;
@@ -41,5 +20,19 @@ class Solution {
             T_pointer--;
         } 
         return true;
+    }
+    private int reformed(String str, int skips, int pointer) {
+        while(pointer >= 0) {
+                if(str.charAt(pointer) == '#') {
+                    skips++;
+                    pointer--;
+                } else if(skips > 0) {
+                    pointer--;
+                    skips--;
+                } else {
+                    return pointer;
+                }
+            }
+        return pointer;
     }
 }
