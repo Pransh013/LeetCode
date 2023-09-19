53b65c3a336b87bd810246d89bbbeb66811f360d
Pranshu Verma <105149813+Pransh013@users.noreply.github.com>
2023-09-19 14:44:48 +0530

    Time: 2 ms (63.02%), Space: 40.9 MB (19.64%) - LeetHub

--- 0844-backspace-string-compare/0844-backspace-string-compare.java
+++ 0844-backspace-string-compare/0844-backspace-string-compare.java
@@ -4,18 +4,13 @@ class Solution {
         return reformed(s).equals(reformed(t));
     }
     private String reformed(String str) {
-        int n = str.length();
-        StringBuilder sb = new StringBuilder();
-        int cnt = 0;
-        for(int i=n-1; i>= 0; i--) {
-            char ch = str.charAt(i);
-            if(ch == '#')
-                cnt++;
-            else if(cnt > 0)
-                cnt--;
-            else
-                sb.insert(0, ch);
+        Stack<Character> st = new Stack();
+        for(char ch : str.toCharArray()) {
+            if(ch != '#')
+                st.push(ch);
+            else if(!st.empty())
+                st.pop();
         }
-        return sb.toString();
+        return String.valueOf(st);
     }
 }
