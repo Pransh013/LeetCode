cdf055837d1424d75b7ffefd438c9d246f1cc8ec
Pranshu Verma <105149813+Pransh013@users.noreply.github.com>
2023-09-20 14:34:46 +0530

    Time: 14 ms (68.35%), Space: 43.3 MB (19.65%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.java
+++ 0016-3sum-closest/0016-3sum-closest.java
@@ -7,21 +7,23 @@ class Solution {
         int nearSum =  nums[0] + nums[1] + nums[2];
         
         for(int i=0; i<n-2; i++) {
-            int j = i + 1, k = n - 1;
-            
-            while(j < k) {
-                int sum = nums[i] + nums[j] + nums[k];
+            if(i == 0 || nums[i] != nums[i-1]) {
                 
-                if(sum == target)
-                    return sum;
-                
-                if(Math.abs(target - sum) < Math.abs(target - nearSum))
-                    nearSum = sum;
-                
-                if(sum < target)
-                    j++;
-                else
-                    k--;
+                int j = i + 1, k = n - 1;
+                while(j < k) {
+                    int sum = nums[i] + nums[j] + nums[k];
+
+                    if(sum == target)
+                        return sum;
+
+                    if(Math.abs(target - sum) < Math.abs(target - nearSum))
+                        nearSum = sum;
+
+                    if(sum < target)
+                        j++;
+                    else
+                        k--;
+                } 
             }
         }
         return nearSum;
