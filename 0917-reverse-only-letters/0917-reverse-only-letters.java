4e1eebed31bb60f79acc48d6eff8a7bab04e4f68
Pranshu Verma <105149813+Pransh013@users.noreply.github.com>
2023-09-21 00:35:37 +0530

    Time: 7 ms (9.64%), Space: 40.9 MB (17.53%) - LeetHub

--- 0917-reverse-only-letters/0917-reverse-only-letters.java
+++ 0917-reverse-only-letters/0917-reverse-only-letters.java
@@ -1,21 +1,19 @@
 class Solution {
-    public String reverseOnlyLetters(String str) {
-        
-        StringBuilder sb = new StringBuilder(str);
-        
-        int s = 0, e = str.length() - 1;
-        
-        while(s < e) {
-            char chL = sb.charAt(s);
-            char chR = sb.charAt(e);
-            if(Character.isLetter(chL) && Character.isLetter(chR)) {
-                sb.setCharAt(s++, chR);
-                sb.setCharAt(e--, chL);
-            } else if(!Character.isLetter(chL))
-                s++;
-            else 
-                e--;
+    public String reverseOnlyLetters(String S) {
+        StringBuilder ans = new StringBuilder();
+        int j = S.length() - 1;
+        for (int i = 0; i < S.length(); ++i) {
+            if (Character.isLetter(S.charAt(i))) {
+                while (!Character.isLetter(S.charAt(j)))
+                    j--;
+                System.out.println(S.charAt(j));
+                ans.append(S.charAt(j--));
+            } else {
+                System.out.println(S.charAt(i));
+                ans.append(S.charAt(i));
+            }
         }
-        return sb.toString();
+
+        return ans.toString();
     }
 }
