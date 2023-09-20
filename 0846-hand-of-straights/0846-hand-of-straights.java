8dec76ea6465401685b1d8e1ed2d1029bfeb5956
Pranshu Verma <105149813+Pransh013@users.noreply.github.com>
2023-09-20 17:34:00 +0530

    Time: 45 ms (40.65%), Space: 44.1 MB (81.66%) - LeetHub

--- 0846-hand-of-straights/0846-hand-of-straights.java
+++ 0846-hand-of-straights/0846-hand-of-straights.java
@@ -4,27 +4,23 @@ class Solution {
         if(hand.length % groupSize != 0) return false;
         
         TreeMap<Integer, Integer> tm = new TreeMap();
-        
         for(int val : hand) {
-            if(tm.containsKey(val)) {
+            if(tm.containsKey(val))
                 tm.replace(val, tm.get(val) + 1);
-            } else
+            else
                 tm.put(val, 1);
         }
         
         while(tm.size() > 0) {
             int curr = tm.firstKey();
-        
             for(int i=curr; i<curr + groupSize; i++) {
                 if(!tm.containsKey(i)) return false;
-                int cnt = tm.get(i);
-                if(cnt == 1) 
+                if(tm.get(i) == 1) 
                     tm.remove(i);
                 else
                     tm.replace(i, tm.get(i) - 1);
             }
         }
-        
         return true;
     }
 }
