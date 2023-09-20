428569bb3d61352be6e76deb136e99de954e3e32
Pranshu Verma <105149813+Pransh013@users.noreply.github.com>
2023-09-21 00:18:40 +0530

    Time: 0 ms (100.00%), Space: 40.6 MB (71.11%) - LeetHub

--- 0771-jewels-and-stones/0771-jewels-and-stones.java
+++ 0771-jewels-and-stones/0771-jewels-and-stones.java
@@ -1,14 +1,11 @@
 class Solution {
-    public int numJewelsInStones(String str1, String str2) {
+    public int numJewelsInStones(String J, String S) {
         int cnt = 0;
-        int[] freqMap = new int[123];
-        for(int i=0; i<str2.length(); i++) {
-            freqMap[str2.charAt(i)]++;
+        for(char ch : S.toCharArray()) {
+            if(J.indexOf(ch) > -1)
+                cnt++;
         }
-        
-        for(int i=0; i<str1.length(); i++) {
-            cnt += freqMap[str1.charAt(i)];
-        }
+
         return cnt;
     }
 }
