52dbc9123c58ae46155d7332068608d5b56f314f
Pranshu Verma <105149813+Pransh013@users.noreply.github.com>
2023-10-06 02:38:15 +0530

    Time: 2 ms (98.52%), Space: 46.6 MB (47.18%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,23 +1,30 @@
 class Solution {
     public List<Integer> majorityElement(int[] arr) {
         ArrayList<Integer> al = new ArrayList<>();
-        HashMap<Integer, Integer> hm = new HashMap<>();
-        
         int n = arr.length;
-        
-        for(int ele : arr) {
-            if(hm.containsKey(ele)) {
-                hm.put(ele, hm.get(ele) + 1);
-            } else {
-                hm.put(ele, 1);
+        int cnt1 = 0, cnt2 = 0, num1 = Integer.MAX_VALUE, num2 = Integer.MAX_VALUE;
+        for(int val : arr) {
+            if(cnt1 == 0 && val != num2) {
+                num1 = val;
+                cnt1 = 1;
+            }   
+            else if(cnt2 == 0 && val != num1) {
+                num2 = val;
+                cnt2 = 1;
+            }
+            else if(val == num1) cnt1++;
+            else if(val == num2)cnt2++;
+            else {
+                cnt1--; cnt2--;
             }
         }
-        
-        for(int key : hm.keySet()) {
-            if(hm.get(key) > n/3) 
-                al.add(key);
+        cnt1 = 0; cnt2 = 0;
+        for(int val : arr) {
+            if(val == num1) cnt1++;
+            if(val == num2) cnt2++;
         }
-        
+        if(cnt1 > n / 3) al.add(num1);
+        if(cnt2 > n / 3) al.add(num2);
         return al;
     }
 }
