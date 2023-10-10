ac6a955024b75e810729140b83119552b1d0bf04
Pranshu Verma <105149813+Pransh013@users.noreply.github.com>
2023-10-10 20:11:31 +0530

    Time: 0 ms (100.00%), Space: 39.5 MB (14.79%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,13 +1,10 @@
 class Solution {
     public int climbStairs(int n) {
-        return countPaths(n, new int[n + 1]);
-    }
-    public int countPaths(int n, int[] dp) {
-        if(n == 0) return 1;
-        if(dp[n] != 0) return dp[n];
-        int path1 = 0, path2 = 0;
-        path1 = countPaths(n - 1, dp);
-        if(n > 1) path2 = countPaths(n - 2, dp);
-        return dp[n] = path1 + path2;
+        int[] dp = new int[n + 1];
+        dp[n] = 1;
+        dp[n - 1] = 1;
+        for(int i = n - 2; i >= 0; i--)
+            dp[i] += dp[i + 1] + dp[i + 2];
+        return dp[0];
     }
 }
