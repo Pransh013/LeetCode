832f34fee1242b8543a23917dba1df0f7ae5c96c
Pranshu Verma <105149813+Pransh013@users.noreply.github.com>
2023-10-10 17:00:24 +0530

    Time: 7 ms (35.63%), Space: 38.9 MB (82.86%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,17 +1,9 @@
 class Solution {
     public int fib(int n) {
-        int a = 0;
-        int b = 1;
-        int c = 0;
-        if(n == 0 || n == 1)
-            return n;
-        else if(n >=2) {
-            for(int i=2; i<=n; i++) {
-                c = a+b;
-                a = b;
-                b = c;
-        }
-        }
-        return  c;
+        return getFib(n);
+    }
+    private int getFib(int n) {
+        if(n == 0 || n == 1) return n;
+        return getFib(n-1) + getFib(n - 2);
     }
 }
