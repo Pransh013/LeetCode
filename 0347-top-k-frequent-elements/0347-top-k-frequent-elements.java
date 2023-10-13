c3455f1d04843137d11689b629f54215e9c3fbaa
Pranshu Verma <105149813+Pransh013@users.noreply.github.com>
2023-10-13 12:57:12 +0530

    Time: 11 ms (88.18%), Space: 47.6 MB (39.33%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,19 +1,24 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
+        int n = nums.length;
         HashMap<Integer, Integer> map = new HashMap<>();
+        for(int val : nums) map.put(val, map.getOrDefault(val, 0) + 1);
         
-        for(int val : nums) 
-            map.put(val, map.getOrDefault(val, 0) + 1);
-        
-        Queue<Integer> pq = new PriorityQueue<>((a, b) -> map.get(a) - map.get(b));
-        
+        ArrayList<Integer>[] bucket = new ArrayList[n + 1];
+        for(int i = 0; i <= n; i++) bucket[i] = new ArrayList<>();
         for(int key : map.keySet()) {
-            pq.add(key);
-            
-            if(pq.size() > k) pq.poll();
+            bucket[map.get(key)].add(key);
         }
         int[] temp = new int[k];
-        for(int i = 0; i < k; i++) temp[i] = pq.remove();
+        int idx = 0;
+        for(int i = n; i >= 0; i--) {
+            if(!bucket[i].isEmpty()) {
+                for(int val : bucket[i]) {
+                    if(idx < k) temp[idx++] = val;
+                }
+            }
+            if(idx == k) break;
+        }
         return temp;
     }
 }
