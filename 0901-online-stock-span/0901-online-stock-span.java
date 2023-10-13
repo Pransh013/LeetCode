6673112e6579d2aa022116b065069526d1726c00
Pranshu Verma <105149813+Pransh013@users.noreply.github.com>
2023-10-13 18:28:31 +0530

    Time: 28 ms (62.23%), Space: 55 MB (38.78%) - LeetHub

--- 0901-online-stock-span/0901-online-stock-span.java
+++ 0901-online-stock-span/0901-online-stock-span.java
@@ -1,15 +1,29 @@
 class StockSpanner {
-    Stack<int[]> st;
+    class Pair {
+        int price;
+        int idx;
+    }
+    
+    Stack<Pair> st;
+    int t = 0;
     public StockSpanner() {
         st = new Stack<>();
+        Pair base = new Pair();
+        base.price = 100000 + 1;
+        base.idx = -1;
+        st.push(base);
     }
     
     public int next(int price) {
-        int sum = 1;
-        while(!st.isEmpty() && st.peek()[0] <= price)
-            sum += st.pop()[1];
-        st.push(new int[]{price, sum});
-        return sum;
+        Pair p = new Pair();
+        p.price = price;
+        p.idx = t;
+        t++;
+        
+        while(st.peek().price <= p.price) st.pop();
+        int ans = p.idx - st.peek().idx;
+        st.push(p);
+        return ans;
     }
 }
 
