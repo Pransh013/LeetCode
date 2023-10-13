1872c1bd4538912b3b5fd40c9701585ed7d67206
Pranshu Verma <105149813+Pransh013@users.noreply.github.com>
2023-10-13 18:40:39 +0530

    Time: 1 ms (74.76%), Space: 40.3 MB (79.08%) - LeetHub

--- 0925-long-pressed-name/0925-long-pressed-name.java
+++ 0925-long-pressed-name/0925-long-pressed-name.java
@@ -1,30 +1,22 @@
 class Solution {
     public boolean isLongPressedName(String name, String typed) {
-        int l1 = name.length(), l2 = typed.length();
-        if((l2 < l1) || (name.charAt(l1 - 1) != typed.charAt(l2 - 1)) || (name.charAt(0) != typed.charAt(0)))
-            return false;
-        
-        int i=0, j=0;
-        char prev = ' ';
-        while(i < l1 && j < l2) {
-            char curr = name.charAt(i);
-            
-            if(typed.charAt(j) == curr) {
-                prev = curr;
-                i++;
-                j++;
+        if(typed.length() < name.length()) return false;
+        int i = 0, j = 0;
+        while(i < name.length()) {
+            if(j == typed.length()) return false;
+            if(name.charAt(i) == typed.charAt(j)) {
+                i ++;
+                j ++;
             } else {
-                if(typed.charAt(j) == prev)
-                    j++;
-                else 
-                    return false;
+                if(j == 0 || typed.charAt(j) != typed.charAt(j - 1)) 
+                    return false; 
+                else j ++;
             }
         }
-        while(j < l2) {
-            if(typed.charAt(j) != typed.charAt(j-1))
-                return false;
-            j++;
+        while(j != typed.length()) {
+            if(typed.charAt(j) != typed.charAt(j - 1)) return false;
+            j ++;
         }
-        return i == l1;
+        return true;
     }
 }
