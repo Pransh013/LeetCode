5a044dadef0308b0f6c7f14bceeb0aebe04c3dad
Pranshu Verma <105149813+Pransh013@users.noreply.github.com>
2023-10-13 21:39:36 +0530

    Time: 7 ms (86.13%), Space: 41.1 MB (91.84%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.java
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -1,37 +1,17 @@
 class Solution {
     public boolean isIsomorphic(String s, String t) {
-        HashMap<Character, Character> map = new HashMap<>();
-        
-        for(int i = 0; i < s.length(); i++) {
-            char ch = s.charAt(i);
-            if(!map.containsKey(ch)) {
-                map.put(ch, t.charAt(i));
-            }
+
+        int map1[]=new int[200];
+        int map2[]=new int[200];
+
+        for(int i=0;i<s.length();i++)
+        {
+            if(map1[s.charAt(i)]!=map2[t.charAt(i)])
+                return false;
+
+            map1[s.charAt(i)]=i+1;
+            map2[t.charAt(i)]=i+1;
         }
-        StringBuilder res = new StringBuilder();
-            
-        for(int i = 0; i <= s.length() - 1; i++) {
-            char ch = s.charAt(i);
-            res.append(map.get(ch));
-        }
-        map.clear();
-         
-        boolean flag1 =  res.toString().equals(t);
-        res.setLength(0);
-        for(int i = 0; i < s.length(); i++) {
-            char ch = t.charAt(i);
-            if(!map.containsKey(ch)) {
-                map.put(ch, s.charAt(i));
-            }
-        }
-            
-        for(int i = 0; i <= s.length() - 1; i++) {
-            char ch = t.charAt(i);
-            res.append(map.get(ch));
-        }
-        
-        boolean flag2 =  res.toString().equals(s);
-        
-        return (flag1 && flag2);
+        return true;
     }
 }
