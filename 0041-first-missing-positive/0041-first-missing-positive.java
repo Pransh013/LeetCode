a5bf4f375e63ffa1231540c8bceb93f6839aea1a
Pranshu Verma <105149813+Pransh013@users.noreply.github.com>
2023-10-19 00:14:04 +0530

    Time: 1 ms (98.71%), Space: 60 MB (11.74%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.java
+++ 0041-first-missing-positive/0041-first-missing-positive.java
@@ -1,29 +1,22 @@
 class Solution {
     public int firstMissingPositive(int[] nums) {
         int n = nums.length;
-        if(n == 1 && nums[0] == 1) return 2;
-        else if(n == 1) return 1;
-        
-        Arrays.sort(nums);
-        if(nums[n - 1] <= 0) return 1;
-        int idx = upperBound(nums, 0);
-        int num = 1;
-        
-        for(int i = idx; i < n; i++) {
-            if(i < n - 1 && nums[i] == nums[i + 1]) continue;
-            if(nums[i] == num) num++;
-            else return num;
+        for(int i = 0; i < n;) {
+            if(nums[i] > 0 && nums[i] < n) {
+                if(nums[i] != nums[nums[i] - 1]) swap(nums, i, nums[i] - 1);
+                else i++;
+            } else i++;
         }
-        return num;
+        
+        for(int i = 0; i < n; i++) 
+            if(nums[i] != i + 1) return (i + 1);
+        
+        return (nums[n - 1] + 1);
     }
     
-    private int upperBound(int[] nums, int k) {
-        int low = 0, high = nums.length - 1;
-        while(low <= high) {
-            int mid = low + (high - low) / 2;
-            if(nums[mid] > k) high = mid - 1;
-            else low = mid + 1;
-        }
-        return low;
+    private void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
 }
