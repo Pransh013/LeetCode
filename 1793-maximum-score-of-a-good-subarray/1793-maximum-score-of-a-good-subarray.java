45fdcb61ebd3b7043de8a09efb4f335af27bfccf
Pranshu Verma <105149813+Pransh013@users.noreply.github.com>
2023-10-23 01:41:51 +0530

    Time: 7 ms (81.90%), Space: 60 MB (5.71%) - LeetHub

--- 1793-maximum-score-of-a-good-subarray/1793-maximum-score-of-a-good-subarray.java
+++ 1793-maximum-score-of-a-good-subarray/1793-maximum-score-of-a-good-subarray.java
@@ -4,15 +4,9 @@ class Solution {
         int score = nums[k];
         int min = nums[k];
         while(i > 0 || j < n - 1) {
-            int left = (i > 0) ? nums[i - 1] : 0;
-            int right = (j < n - 1) ? nums[j + 1] : 0;
-            if(left < right) {
-                j++;
-                min = Math.min(min, right);
-            } else {
-                i--;
-                min = Math.min(min, left);
-            }
+            if(((i > 0) ? nums[i - 1] : 0) < ((j < n - 1) ? nums[j + 1] : 0)) 
+                min = Math.min(min, nums[++j]);
+            else min = Math.min(min, nums[--i]);
             score = Math.max(score, min * (j - i + 1));
         }
         return score;
