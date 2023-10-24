1adc20b99f605980080f9e9fea77e5ebc41d512a
Pranshu Verma <105149813+Pransh013@users.noreply.github.com>
2023-10-24 15:28:09 +0530

    Time: 1 ms (89.86%), Space: 44.2 MB (94.19%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,21 +1,30 @@
 class Solution {
     public void setZeroes(int[][] a) {
         int n = a.length, m = a[0].length;
-        
-        boolean[] rows = new boolean[n];
-        boolean[] cols = new boolean[m];
+        int[][] b= new int[n][m];
         
         for(int r = 0; r < n; r++) {
             for(int c = 0; c < m; c++) {
                 if(a[r][c] == 0) {
-                    rows[r] = true;
-                    cols[c] = true;
+                    fillRow(b, r);
+                    fillCol(b, c);
                 }
             }
         }
-        
-        for(int i = 0; i < n; i++)
-            for(int j = 0; j < m; j++)
-                if(rows[i] || cols[j]) a[i][j] = 0;
+        for(int r = 0; r < n; r++) {
+            for(int c = 0; c < m; c++) {
+                if(b[r][c] == 1) {
+                    a[r][c] = 0;
+                }
+            }
+        }
+    }
+    
+    private void fillRow(int[][] a, int r) {
+        for(int c = 0; c < a[0].length; c++) a[r][c] = 1;
+    }
+    
+    private void fillCol(int[][] a, int c) {
+        for(int r = 0; r < a.length; r++) a[r][c] = 1;
     }
 }
