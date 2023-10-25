759a62298010aeb0ce1e5cd0e23cdb46a2390290
Pranshu Verma <105149813+Pransh013@users.noreply.github.com>
2023-10-25 17:51:20 +0530

    Time: 101 ms (8.30%), Space: 41.1 MB (70.83%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.java
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.java
@@ -4,12 +4,14 @@ class Solution {
         int[][] dp = new int[k + 1][n];
         for (int r = 1; r <= k; r++) {
             for (int c = 1; c < n; c++) {
-                dp[r][c] = dp[r][c - 1];
+                int max = Integer.MIN_VALUE;
+                max = Math.max(max, dp[r][c - 1]);
                 for (int i = c - 1; i >= 0; i--) {
                     int curr = dp[r - 1][i] + a[c] - a[i];
-                    if (curr > dp[r][c])
-                        dp[r][c] = curr;
+                    if (curr > max)
+                        max = curr;
                 }
+                dp[r][c] = max;
             }
         }
         return dp[k][n - 1];
