fe20d4885feea8f02f4008c186b4fd86e76eba3a
Pranshu Verma <105149813+Pransh013@users.noreply.github.com>
2023-11-01 12:31:24 +0530

    Time: 0 ms (100.00%), Space: 41.3 MB (29.83%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.java
+++ 0048-rotate-image/0048-rotate-image.java
@@ -1,26 +1,27 @@
 class Solution {
     public void rotate(int[][] arr) {
         int n = arr.length;
-        
+        for(int i = 0; i < n; i++) {
+            for(int j = 0 ; j < i; j++) {
+                int temp = arr[i][j];
+                arr[i][j] = arr[j][i];
+                arr[j][i] = temp;
+            }
+        }
+        for(int[] row : arr) {
+            revRow(row);
+        }
+    }
     
-    for (int i = 0; i < arr.length; i++) {
-      for (int j = 0; j < i; j++) {
-        int temp = arr[i][j];
-        arr[i][j] = arr[j][i];
-        arr[j][i] = temp;
-      }
+    private void revRow(int[] row) {
+        int left = 0, right = row.length - 1;
+        
+        while(left < right) {
+            int temp = row[left];
+            row[left] = row[right];
+            row[right] = temp;
+            left++;
+            right--;
+        }
     }
-    for (int i = 0; i < arr.length; i++) {
-      int li = 0;
-      int ri = arr[0].length - 1;
-      while (li <= ri) {
-        int temp = arr[i][li];
-        arr[i][li] = arr[i][ri];
-        arr[i][ri] = temp;
-
-        li++;
-        ri--;
-      }
-    }
-  }
 }
