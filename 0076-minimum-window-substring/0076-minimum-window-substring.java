af50f4d50ea3df2073b2058295b831e4d981f713
Pranshu Verma <105149813+Pransh013@users.noreply.github.com>
2023-11-03 13:56:41 +0530

    Time: 405 ms (5.03%), Space: 43.9 MB (59.07%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,32 +1,35 @@
 class Solution {
     public String minWindow(String s, String t) {
         if(t.length() > s.length()) return "";
+        
         int k = t.length();
         String ans = "";
         HashMap<Character, Integer> sMap = new HashMap<>();
         HashMap<Character, Integer> tMap = new HashMap<>();
-        for(char ch : t.toCharArray()) tMap.put(ch, tMap.getOrDefault(ch, 0) + 1);
-        for(int i = 0, j = 0; j < s.length();) {
-            char ch = s.charAt(j);
-            sMap.put(ch, sMap.getOrDefault(ch, 0) + 1);
+        
+        for(char c : t.toCharArray()) tMap.put(c, tMap.getOrDefault(c, 0) + 1);
+        
+        for(int i = 0, j = 0; j < s.length(); j++) {
+            sMap.put(s.charAt(j), sMap.getOrDefault(s.charAt(j), 0) + 1);
+            
             if(isFeasible(sMap, tMap)) {
                 while(isFeasible(sMap, tMap)) {
-                    sMap.put(s.charAt(i), sMap.get(s.charAt(i)) - 1);
+                    sMap.put(s.charAt(i), sMap.getOrDefault(s.charAt(i), 0) - 1);
                     i++;
                 }
                 i--;
-                sMap.put(s.charAt(i), sMap.get(s.charAt(i)) + 1);
-                if(ans.equals("") || s.substring(i, j + 1).length() < ans.length())
+                sMap.put(s.charAt(i), sMap.getOrDefault(s.charAt(i), 0) + 1);
+                if(ans.length() == 0 || s.substring(i, j + 1).length() < ans.length())
                     ans = s.substring(i, j + 1);
             }
-            j++;
         } 
         return ans;
     }
+    
     private boolean isFeasible(HashMap<Character, Integer> sMap, HashMap<Character, Integer> tMap) {
         for(char key : tMap.keySet()) {
             if(!sMap.containsKey(key) || sMap.get(key) < tMap.get(key)) return false;
         }
-        return true;
+       return true;
     }
 }
