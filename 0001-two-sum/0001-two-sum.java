942e0fa5893706f10fc6bb3a2177ed72c785f29f
Pranshu Verma <105149813+Pransh013@users.noreply.github.com>
2023-11-22 18:14:11 +0530

    Time: 7 ms (55.59%), Space: 44.4 MB (5.19%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,16 +1,32 @@
 class Solution {
-    public int[] twoSum(int[] nums, int target) {
-        int n = nums.length;
-        int[] ans = new int[2];
-        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
-        for(int i=0; i<n; i++) {
-            if(map.containsKey(target - nums[i])) {
-                ans[1] = i;
-                ans[0] = map.get(target - nums[i]);
-                return ans;
-            }
-            map.put(nums[i], i);
+    private class Pair implements Comparable<Pair> {
+        int val;
+        int idx;
+        
+        Pair(int val, int idx) {
+            this.val = val;
+            this.idx = idx;
         }
-        return ans;
+        public int compareTo(Pair temp) {
+            return this.val - temp.val;
+        }
+    }
+    public int[] twoSum(int[] nums, int target) {
+        Pair[] pairs = new Pair[nums.length];
+        for(int i = 0; i < nums.length; i++) {
+            pairs[i] = new Pair(nums[i], i);
+        }
+        
+        Arrays.sort(pairs);
+        for(int i = 0, j = pairs.length - 1; i < j;) {
+            int a = pairs[i].val;
+            int b = pairs[j].val;
+            
+            if(a + b == target) {
+                return new int[] {pairs[i].idx, pairs[j].idx};
+            } else if(a + b > target) j--;
+            else i++;
+        }
+        return new int[0]; 
     }
 }
