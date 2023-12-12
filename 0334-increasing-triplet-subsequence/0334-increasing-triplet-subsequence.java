e1b8f76d4cfb98c89889bfd4aafc8be8290d0ac5
Pranshu Verma <105149813+Pransh013@users.noreply.github.com>
2023-12-12 12:41:11 +0530

    Time: 6 ms (8.48%), Space: 125.3 MB (93.98%) - LeetHub

--- 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.java
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.java
@@ -9,10 +9,9 @@ class Solution {
         
         for(int i = 1; i < nums.length; i++) {
             prev[i] = (prev[i - 1] < nums[i]) ? prev[i - 1] : nums[i];
-        }
-        
-        for(int i = nums.length - 2; i >= 0; i--) {
-            next[i] = (next[i + 1] > nums[i]) ? next[i + 1] : nums[i];
+            
+            next[nums.length - 1 - i] = (next[nums.length - i] > nums[nums.length - 1 - i]) ? 
+            next[nums.length - i] : nums[nums.length - 1 - i];
         }
         
         for(int i = 0; i < nums.length; i++) {
