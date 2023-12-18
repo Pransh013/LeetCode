061f4a1e130301c6d6fe6d490e40523c75e6d082
Pranshu Verma <105149813+Pransh013@users.noreply.github.com>
2023-12-18 19:30:16 +0530

    Time: 2 ms (97.51%), Space: 43.8 MB (84.63%) - LeetHub

--- 1913-maximum-product-difference-between-two-pairs/1913-maximum-product-difference-between-two-pairs.java
+++ 1913-maximum-product-difference-between-two-pairs/1913-maximum-product-difference-between-two-pairs.java
@@ -1,7 +1,19 @@
 class Solution {
     public int maxProductDifference(int[] nums) {
-        int n = nums.length;
-        Arrays.sort(nums);
-        return nums[n - 1] * nums[n - 2] - nums[0] * nums[1];
+        int max = 0, smax = 0;
+        int min = 10001, smin = 10001;
+        
+        for(int val : nums) {
+            if(val > max) {
+                smax = max;
+                max = val;
+            } else if(val > smax) smax = val;
+            
+            if(val < min) {
+                smin = min;
+                min = val;
+            } else if(val < smin) smin = val;
+        }
+        return max * smax - min * smin;
     }
 }
