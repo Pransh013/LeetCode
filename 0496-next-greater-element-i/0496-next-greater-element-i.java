2f5a6b4de7d2e6c4540f48e75d6f403ce38cefe2
Pranshu Verma <105149813+Pransh013@users.noreply.github.com>
2024-01-02 01:02:27 +0530

    Time: 6 ms (15.68%), Space: 43.1 MB (80.70%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,18 +1,29 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        Map<Integer, Integer> map = new HashMap<>();
+        
+        for(int i = 0; i < nums2.length; i++) {
+            map.put(nums2[i], i);
+        }
+        
+        Stack<Integer> st = new Stack<>();
+        
+        for(int i = 0; i < nums2.length; i++) {
+            if(st.size() > 0 && nums2[i] > nums2[st.peek()]) {
+                while(st.size() > 0 && nums2[i] > nums2[st.peek()]) {
+                    nums2[st.peek()] = nums2[i];
+                    st.pop();
+                }
+            }
+            st.push(i);
+        }
+        
+        while(st.size() > 0) {
+            nums2[st.pop()] = -1;
+        }
         
         for(int i = 0; i < nums1.length; i++) {
-            Stack<Integer> st = new Stack<>();
-            for(int j = 0; j < nums2.length; j++) {
-                st.push(nums2[j]);
-            }
-            int max = Integer.MIN_VALUE;
-            while(st.peek() != nums1[i]) {
-                if(st.peek() > nums1[i]) max = st.peek();
-                st.pop();
-            }
-            if(max == Integer.MIN_VALUE) max = -1;
-            nums1[i] = max;
+            nums1[i] = nums2[map.get(nums1[i])];
         }
         return nums1;
     }
