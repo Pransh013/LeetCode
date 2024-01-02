0dfff595da91583db2d0db0fa3f952ba59cae698
Pranshu Verma <105149813+Pransh013@users.noreply.github.com>
2024-01-03 02:04:37 +0530

    Time: 0 ms (100.00%), Space: 42.7 MB (6.97%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -11,39 +11,33 @@
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
         
-        ListNode node1 = list1;
-        ListNode node2 = list2;
+        if(list1 == null) return list2;
+        if(list2 == null) return list1;
         
         ListNode head = null;
         ListNode tail = null;
         
-        while(node1 != null && node2 != null) {
-            if(node1.val < node2.val) {
-                if(head == null) head = node1;
-                if(tail != null) tail.next = node1;
-                tail = node1;
-                node1 = node1.next;
+        while(list1 != null && list2 != null) {
+            ListNode temp = null;
+            if(list1.val < list2.val) {
+                temp = list1;
+                list1 = list1.next;
             } else {
-                if(head == null) head = node2;
-                if(tail != null) tail.next = node2;
-                tail = node2;
-                node2 = node2.next;
+                temp = list2;
+                list2 = list2.next;
+            }
+            
+            temp.next = null;
+            if(head == null) head = tail = temp;
+            else {
+                tail.next = temp;
+                tail = temp;
             }
         }
         
-        while(node1 != null) {
-            if(head == null) head = node1;
-            if(tail != null) tail.next = node1;
-            tail = node1;
-            node1 = node1.next;
-        }
+        if(list1 == null) tail.next = list2;
+        if(list2 == null) tail.next = list1;        
         
-        while(node2 != null) {
-            if(head == null) head = node2;
-            if(tail != null) tail.next = node2;
-            tail = node2;
-            node2 = node2.next;
-        }
         return head;
     }
 }
