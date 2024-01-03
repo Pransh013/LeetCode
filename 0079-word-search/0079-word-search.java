90b96b92d448f2ee9ccb7038046db9d2e54ace94
Pranshu Verma <105149813+Pransh013@users.noreply.github.com>
2024-01-04 01:09:49 +0530

    Time: 153 ms (52.51%), Space: 41.7 MB (26.43%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -8,7 +8,8 @@ class Solution {
         
         for(int i = 0; i < board.length; i++) {
             for(int j = 0; j < board[0].length; j++) {
-                if(search(board, i, j, 0, s, vis)) return true;
+                if(board[i][j] == word.charAt(0))
+                    if(search(board, i, j, 0, s, vis)) return true;
             }
         }
         return false;
